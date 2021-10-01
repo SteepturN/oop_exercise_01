bf14634919dee338bacaa46f362f57466864a3b4
admin <admin@gg>
2021-10-01 17:28:24 +0300

    very important

--- main.cpp
+++ main.cpp
@@ -8,18 +8,19 @@ int main(){
 	char ch(' ');
 	char command[20];
 	do{
-
-
 		do ch = getchar(); while(ch == ' ' || ch == '\n' || ch == '\t');
 		if(ch == EOF) return 0;
 		else ungetc(ch, stdin);
 		scanf("%s", command);
 		if(strcmp(command,"c")==0){
 			scanf("%lf%lf", &re1, &im1);
+			std::cout << "conj" << '(' << re1 << ',' << im1 << ')' << " = ";
 			Complex(re1, im1).conj().print();
 		}
+		else if(strcmp(command, "exit")==0) return 0;
 		else{
 			scanf("%lf%lf%lf%lf", &re1, &im1, &re2, &im2);
+			std::cout << '(' << re1 << ',' << im1 << ')' << command << '(' << re2 << ',' << im2 << ')' << " = ";
 			if(strcmp(command,"a")==0){
 				Complex(re1, im1).add(Complex(re2, im2)).print();
 			}
@@ -33,17 +34,16 @@ int main(){
 				Complex(re1, im1).div(Complex(re2, im2)).print();
 			}
 			else if(strcmp(command,"e")==0){
-				std::cout << Complex(re1, im1).equ(Complex(re2, im2));
+				std::cout << Complex(re1, im1).equ(Complex(re2, im2)) << std::endl;
 			}
 			else if(strcmp(command,"bth")==0){
 				std::cout << Complex(re1, im1).bth(Complex(re2, im2)) << std::endl;
 			}
-			else if(strcmp(command, "exit")==0) return 0;
 			else{
 				std::cout << "wrong input" << std::endl;
 				while((ch = getchar()) != '\n');
 			}
 		}
-	} while(strcmp(command,"exit"));
+	} while(true);
 	return 0;
 }
