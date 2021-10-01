7405d6a3ec66b7c637d6ed2694522126d8623dcd
admin <admin@gg>
2021-10-01 18:32:21 +0300

    lets try

--- build/CMakeFiles/Makefile.cmake
+++ build/CMakeFiles/Makefile.cmake
@@ -45,5 +45,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/main.dir/DependInfo.cmake"
+  "CMakeFiles/oop_exercise_01.dir/DependInfo.cmake"
   )
