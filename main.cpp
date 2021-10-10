bf2790d815d131a5453448f24770926c6d3e50d9
admin <admin@gg>
2021-10-10 13:02:12 +0300

    very well done input

--- main.cpp
+++ main.cpp
@@ -3,73 +3,53 @@
 #include "Complex.cpp"
 #include <cstdio>
 #include <set>
+#include "Read_input.cpp"
 
 int main(){
-
 	Complex c1(0,0), c2(0,0);
 	char ch(' ');
 	char command[20] = {0};
+	bool exit;
 	std::set<std::string> valid_commands = {"c", "a", "s", "m", "d", "e", "bth", "exit"};
-	do{
-		if(get_command(valid_commands, command) == EOF) return 0;
-		while(get_double(c1.a) && get_double(c1.b));
-		if(command != "c") while(get_double(c2.a) && get_double(c2.b));
-		if(command == "bth"){
-			if(get_complex_num(c1) == EOF || get_complex_num(c2) == EOF) return 0;
-			std::cout << Complex(re1, im1).bth(Complex(re2, im2)) << std::endl;
-		}
-		else if(command == "e") {
-			std::cout << Complex(re1, im1).equ(Complex(re2, im2)) << std::endl;
-		}
-		else if(command == "exit") return 0;
-		else {
-			Complex(re1, im1).exec(command, Complex(re2, im2)).print();
-		}
-	} while(strcmp(command,"exit"));
-	return 0;
-}
-
-char get_command(std::set<const char*>& valid_commands, std::string& command)
-{
-	char ch(0);
-	while(true)
-	{
-		std::cin >> ch;
-		if(ch == EOF) return EOF;
-		else ungetc(ch, stdin);
-		std::cin >> command;
-		if(valid_commands.count(command)) return 0;
-		else {
-			std::cin.ignore(10000, '\n')
+	do {
+		exit = false;
+		do{
+			read_return_t answer = get_command(valid_commands, command);
+			switch(answer) {
+				case END_OF_FILE: return 0;
+				case END_OF_LINE: continue;
+				case VALID_INPUT: exit = true; break;
+				case INVALID_INPUT:
+					do ch=getchar(); while((ch != EOF) && (ch != '\n'));
+					std::cout << "wrong input" << std::endl;
+					if(ch == EOF) return 0;
+					else break;
+				}
+		} while(!exit);
+		if(static_cast<std::string>(command) == "exit") return 0;
+		if((get_double(c1.a) != VALID_INPUT) || (get_double(c1.b) != VALID_INPUT)) {
+			do ch = getchar(); while((ch != EOF) && (ch != '\n'));
 			std::cout << "wrong input" << std::endl;
+			if(ch == EOF) return 0;
+			continue;
 		}
-	}
-}
-char get_complex_num(Complex& c1)
-{
-	double a(0), b(0);
-	char ch(0);
-	//can be better< have no time
-	std::cin >> ch;
-	if(ch == EOF) return EOF;
-	else ungetc(ch, stdin);
-	std::cin >> a;
-	if(std::cin.fail())
-	{
-		std::cin.clear();
-		std::cin.ignore(10000, '\n');
-		std::cout << "wrong input\n";
-		
-
-	}
-	std::cin >> b;
-        if(std::cin.fail())
-        {
-                std::cin.clear();
-                std::cin.ignore(10000, '\n');
-                std::cout << "wrong input\n";
-        }
-	c1.a = a; c1.b = b;
+		if(static_cast<std::string>(command) != "c") {
+			if((get_double(c2.a) != VALID_INPUT) || (get_double(c2.b) != VALID_INPUT)) {
+				do ch=getchar(); while((ch != EOF) && (ch != '\n'));
+				std::cout << "wrong input" << std::endl;
+				if(ch == EOF) return 0;
+				continue;
+			}
+		}
+		if(static_cast<std::string>(command) == "c") {
+			c1.conj().print();
+		} else if (static_cast<std::string>(command) == "bth") {
+			std::cout << c1.bth(c2) << std::endl;
+		} else if(static_cast<std::string>(command) == "e") {
+			std::cout << c1.equ(c2) << std::endl;
+		} else {
+			c1.exec(command, c2).print();
+		}
+	} while(true);
 	return 0;
 }
-
